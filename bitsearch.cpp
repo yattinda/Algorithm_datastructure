80394d85bb8bd730f95b55b9de113e038122a272
yattinda <yattinda@gmail.com>
2021-05-10 22:25:51 +0900

    Refactor

--- bitsearch.cpp
+++ bitsearch.cpp
@@ -12,11 +12,11 @@ int main(){
   }
   bool exist = false;
   //bitは2^n通り
-  for(int = 0; bit < (1 << n); ++bit){
+  for(bit = 0; bit < (1 << n); ++bit){
     //部分集合に含まれる要素の和
     int sum = 0;
     for(j = 0; j < n; ++j){
-      //j番目の要素a[j]が部分集合に含まれているかどうか
+      //j番目の要素a[j]が部分集合に含まれているかどうか（左シフト）
       if(bit & (1 << j)){
         sum += a[j];
       }
