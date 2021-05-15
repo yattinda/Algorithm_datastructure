a861deb72cd5d1391a1ad4a8245b9fb7db4153bb
yattinda <yattinda@gmail.com>
2021-05-15 23:01:39 +0900

    Left

---
+++ answer/3-6.cpp
@@ -0,0 +1,14 @@
+//O(k^3)??
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int m, i;
+    cin >> m;
+    vector <int> a(m);
+    
+    for (i = 0; i < m; i++){
+        cin >> a[i];
+    }
+}
