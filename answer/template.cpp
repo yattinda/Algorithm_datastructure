e3dcd2df793d67d2737ec61f5c0ac950e8ab190a
yattinda <yattinda@gmail.com>
2021-05-15 23:01:57 +0900

    Create template file

---
+++ answer/template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int m, i;
+    cin >> m;
+    vector <int> a(m);
+    
+    for (i = 0; i < m; i++){
+        cin >> a[i];
+    }
+}
