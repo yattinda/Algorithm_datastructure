6be1657c28b717de2c9f418c36da4e1f4bc338a5
yattinda <yattinda@gmail.com>
2021-05-15 22:47:49 +0900

    Done

--- answer/3-5.cpp
+++ answer/3-5.cpp
@@ -1,1 +1,30 @@
-#wakaran
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int min_devide(int n){
+    int i = 0;
+    for(;n % 2 == 0; n = n / 2){
+        
+        ++i;
+    }
+    return i;
+}
+
+int main(){
+    int m, i, j, times = 999999;
+    cin >> m;
+    vector <int> a(m);
+    
+    for (i = 0; i < m; i++){
+        cin >> a[i];
+    }
+
+    for (j = 0; j < m; j++){
+        if(min_devide(a[j]) < times){
+            times = min_devide(a[j]);
+        }
+    }
+
+    cout << times << endl;
+}
