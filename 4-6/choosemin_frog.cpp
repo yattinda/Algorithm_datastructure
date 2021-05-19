c8257773a599dcaf29226870f3701a95a58c611b
yattinda <yattinda@gmail.com>
2021-05-19 18:54:58 +0900

    Add comment

--- 4-6/choosemin_frog.cpp
+++ 4-6/choosemin_frog.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 const long long INF = 9999999;
 
+//aは書き換える必要があるので参照渡し,bは比較するだけで書き換える必要がないため値渡し
 template<class T> void chmin(T& a, T b){
     if(a > b){
         a = b;
@@ -21,7 +22,7 @@ int main(){
     vector<long long> dp(n, INF);
 
     dp[0] = 0;
-
+    //帰納法的な考え方
     for (j = 1; j < n; j++){
         chmin(dp[j], dp[j - 1] + abs(h[j] - h[j - 1]));
         if(i > 1){
